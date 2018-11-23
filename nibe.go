d38fe693f20e9987fd7c528fd7c0a977d73305bd
Jonas Liljestrand <jonas.liljestrand@gmail.com>
2018-11-23 20:28:10 +0100

    Initial commit

---
+++ nibe.go
@@ -0,0 +1,25 @@
+package nibe
+
+import (
+	"os"
+)
+
+const nibeUplinkAPI = "https://api.nibeuplink.com"
+
+// Credentials - struct for accessing credentials
+type Credentials struct {
+	ClientSecret    string
+	ClientID        string
+	OAuhRedirectURI string
+	Endpoint        string
+}
+
+// NewCredentials - Helper for settings common values
+func NewCredentials(OAuthredirectURI string) *Credentials {
+	return &Credentials{
+		ClientSecret:    os.Getenv("CLIENT_SECRET"),
+		ClientID:        os.Getenv("CLIENT_ID"),
+		OAuhRedirectURI: OAuhRedirectURI,
+		Endpoint:        nibeUplinkAPI,
+	}
+}
